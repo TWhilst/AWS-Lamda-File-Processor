a58d3eb71da959a69fb7da197cda3fd922faa324
TWhilst <tochethestrict@gmail.com>
2025-06-10 10:42:17 +0100

    made changes

--- terraform/key-pair.tf
+++ terraform/key-pair.tf
@@ -1,5 +1,5 @@
 /// Key Pair
-# resource "aws_key_pair" "Project1_key" {
-#   key_name   = "toche-key1"
-#   public_key = file("~/.ssh/toche-key1.pub") # Path to your public key
-# }
+resource "aws_key_pair" "Project1_key" {
+  key_name   = "toche-key1"
+  public_key = file("~/.ssh/toche-key1.pub") # Path to your public key
+}
